33867b1a16f1bc85b909ff6d8261658ae99ab8f9
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-06 23:06:59 -0500

    apps dto

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,27 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
+
+export class LoginDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}

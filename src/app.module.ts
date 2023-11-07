55be41104eade27642445abd1f9012e33cd360d3
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-06 20:59:02 -0500

    add app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { BookmarkService } from './bookmark/bookmark.service';
+import { BookmarkController } from './bookmark/bookmark.controller';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    PrismaModule,
+  ],
+  providers: [BookmarkService],
+  controllers: [BookmarkController],
 })
 export class AppModule {}

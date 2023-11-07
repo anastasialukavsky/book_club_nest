37c0a19769372a6e4996e0fa86eccd502b12a788
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-06 23:06:46 -0500

    bookmark module

---
+++ src/bookmark/bookmark.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { BookmarkService } from './bookmark.service';
+import { BookmarkController } from './bookmark.controller';
+
+@Module({ providers: [BookmarkService], controllers: [BookmarkController] })
+export class BookmarkModule {}

3e58a8cd14e33845b71662b0f73f36d7244852b7
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-07 23:10:12 -0500

    refactor

--- src/bookmark/bookmark.module.ts
+++ src/bookmark/bookmark.module.ts
@@ -2,5 +2,5 @@ import { Module } from '@nestjs/common';
 import { BookmarkService } from './bookmark.service';
 import { BookmarkController } from './bookmark.controller';
 
-@Module({ providers: [BookmarkService], controllers: [BookmarkController] })
+@Module({ controllers: [BookmarkController], providers: [BookmarkService] })
 export class BookmarkModule {}

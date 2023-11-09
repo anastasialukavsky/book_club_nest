4c29b2c0e43c952b1471ee0b642988e52520cccb
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-08 21:23:54 -0500

    init commit for next js

---
+++ my/src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { Button } from '../../../@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <Button>hello</Button>
+      <p className="text-red-300 text-9xl">hello</p>
+    </div>
+  );
+}

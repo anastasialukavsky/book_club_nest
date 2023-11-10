0c3d1dd8dca9b5179ce5910ab3a8d4db61841bb0
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-09 23:23:29 -0500

    commit

--- my/src/app/page.tsx
+++ my/src/app/page.tsx
@@ -1,10 +1,8 @@
 'use client';
-import { Button } from '../../../@/components/ui/button';
 
 export default function Home() {
   return (
     <div>
-      <Button>hello</Button>
       <p className="text-red-300 text-9xl">hello</p>
     </div>
   );

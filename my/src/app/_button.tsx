f7f67201670874e9f8667c9f54d8611aa01be83f
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-10 22:42:28 -0500

    keeping ft component inactive for now

--- my/src/app/_button.tsx
+++ my/src/app/_button.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
+// import React from 'react';
 
-export default function Button({ props }) {
-  return <div>{props.children}</div>;
-}
+// export default function Button({ props }) {
+//   return <div>{props.children}</div>;
+// }
--- my/src/app/page.tsx
+++ my/src/app/page.tsx
@@ -1,9 +1,9 @@
-'use client';
+// 'use client';
 
-export default function Home() {
-  return (
-    <div>
-      <p className="text-red-300 text-9xl">hello</p>
-    </div>
-  );
-}
+// export default function Home() {
+//   return (
+//     <div>
+//       <p className="text-red-300 text-9xl">hello</p>
+//     </div>
+//   );
+// }

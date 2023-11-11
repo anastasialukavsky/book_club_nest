edf1f2d2fb2a98880dc23391cb14df3d7b0db7cd
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-10 22:44:51 -0500

    add guarded crud logic for project

---
+++ src/project/project.service.ts
@@ -0,0 +1,85 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProjectDto, EditProjectDto } from './dto';
+
+@Injectable()
+export class ProjectService {
+  constructor(private prisma: PrismaService) {}
+
+  async getProjects(userId: string) {
+    const projects = await this.prisma.project.findMany({
+      where: {
+        userId,
+      },
+    });
+
+    return projects;
+  }
+
+  async getProjectById(userId: string, projectId: number) {
+    const project = await this.prisma.project.findFirst({
+      where: {
+        id: projectId,
+        userId,
+      },
+    });
+
+    return project;
+  }
+
+  async createProject(userId: string, dto: CreateProjectDto) {
+    const projectToCreate = await this.prisma.project.create({
+      data: {
+        userId,
+        ...dto,
+      },
+    });
+
+    return projectToCreate;
+  }
+
+  async editProjectById(
+    userId: string,
+    projectId: number,
+    dto: EditProjectDto,
+  ) {
+    const project = await this.prisma.project.findUnique({
+      where: {
+        id: projectId,
+      },
+    });
+
+    if (!project || project.userId !== userId)
+      throw new ForbiddenException(
+        'Access denied: project does not exist, or user provided invalid credentials',
+      );
+
+    return this.prisma.project.update({
+      where: {
+        id: projectId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+  }
+
+  async deleteProjectById(userId: string, projectId: number) {
+    const project = await this.prisma.project.findUnique({
+      where: {
+        id: projectId,
+      },
+    });
+
+    if (!project || project.userId !== userId)
+      throw new ForbiddenException(
+        'Access denied; Project does not exist or user provided invalid credentials',
+      );
+
+    return await this.prisma.project.delete({
+      where: {
+        id: projectId,
+      },
+    });
+  }
+}

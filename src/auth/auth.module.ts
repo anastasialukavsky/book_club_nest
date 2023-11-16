b5da144a7f39c36d17e27e455b67c4c6bda0ceef
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-15 23:19:41 -0500

    register sesson in passport module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { AuthService } from './auth.service';
 // import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy, RtStrategy } from './startegy';
+import { PassportModule } from '@nestjs/passport';
+// import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [JwtModule.register({})],
+  imports: [JwtModule.register({}), PassportModule.register({ session: true })],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy, RtStrategy],
 })

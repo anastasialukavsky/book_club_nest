4f7b50cc9945ff622ea5cc34ab1dfcdfaf555607
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-15 23:18:41 -0500

    add basic session config

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+      //TODO set to true once get HTTPS, setup 'trust proxy' in express
+      // secure: false,
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
   await app.listen(3333);
 }
 bootstrap();

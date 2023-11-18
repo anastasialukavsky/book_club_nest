63c4bc448dd68d20e396dd3e9cb353fcd0d78247
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-18 16:00:35 -0500

    release unused jwt referrence, change returnto done()

--- src/auth/startegy/google.strategy.ts
+++ src/auth/startegy/google.strategy.ts
@@ -1,31 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-// import { User } from '@prisma/client';
-import { ExtractJwt, Strategy, VerifyCallback } from 'passport-jwt';
-// import { config } from 'dotenv';
-// import { User } from '@prisma/client';
-
-// config();
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(config: ConfigService) {
-    // const jwtSecret = config.get('JWT_SECRET');
-    // if (!jwtSecret) {
-    //   throw new Error('JWT_SECRET is not defined in the configuration.');
-    // }
     super({
       clientID: config.get('GOOGLE_CLIENT_ID'),
       clientSecret: config.get('GOOGLE_SECRET'),
-      callbackURL: 'http://localhost:3333/google/redirect',
+      callbackURL: 'http://localhost:3333/auth/google/redirect/',
       scope: ['email', 'profile'],
-      // jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      // secretOrKey: jwtSecret,
     });
   }
 
-  async validateGoogleUser(
+  async validate(
     _access_token: string,
     _refresh_token: string,
     profile: any,
@@ -39,8 +28,8 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         lastName: lastName,
         _access_token,
       };
-      // done(null, user);
-      return user;
+      done(null, user);
+      // return user;
     } catch (err) {
       done(null, err);
     }

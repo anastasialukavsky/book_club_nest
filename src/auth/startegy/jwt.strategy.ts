58cb2b804527f106dee976ffc3fd0b2d793e72f1
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-18 20:05:47 -0500

    rm logs

--- src/auth/startegy/jwt.strategy.ts
+++ src/auth/startegy/jwt.strategy.ts
@@ -12,7 +12,6 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     private prisma: PrismaService,
   ) {
     const jwtSecret = config.get('JWT_SECRET');
-    console.log({ jwtSecret });
     if (!jwtSecret) {
       throw new Error('JWT_SECRET is not defined in the configuration.');
     }
@@ -31,7 +30,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 
     if (!user)
       throw new ForbiddenException('Invalid credentials: user does not exist');
-    // delete user.password;
+
     return user;
   }
 }

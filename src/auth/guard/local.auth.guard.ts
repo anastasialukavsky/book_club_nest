461d5dda20c352c757b85ce62d4a0bd94872a191
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-18 20:05:11 -0500

    rm logs

--- src/auth/guard/local.auth.guard.ts
+++ src/auth/guard/local.auth.guard.ts
@@ -4,13 +4,10 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {
   async canActivate(context: ExecutionContext) {
-    console.log(this.canActivate.prototype);
-    console.log({ context });
-    console.log('hi from local auth guard');
     const result = (await super.canActivate(context)) as boolean;
-    console.log({ result });
+
     const request = context.switchToHttp().getRequest();
-    console.log({ request });
+
     await super.logIn(request);
     return result;
   }

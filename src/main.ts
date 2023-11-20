5ef2064218ad119130c2224c06e214c64ee98cff
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-19 22:41:28 -0500

    add cookieParser

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NotFoundException, ValidationPipe } from '@nestjs/common';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,8 @@ async function bootstrap() {
       saveUninitialized: false,
     }),
   );
+
+  app.use(cookieParser());
 
   app.use(passport.initialize());
   app.use(passport.session());

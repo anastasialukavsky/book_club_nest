4cc3f804061c1476a7421097b1c4f4206d7ec1e9
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-22 17:29:07 -0500

    simplify return to true if email exists and false if it doesnt

--- my/_utilHelpers.ts
+++ my/_utilHelpers.ts
@@ -5,11 +5,13 @@ const HTTP_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
 export async function emailCheck(email: string) {
   try {
     const { data } = await axios.get(
-      `${HTTP_ENDPOINT}/auth/verify-email/${email}}`,
-      // { withCredentials: true },
+      `${HTTP_ENDPOINT}/auth/verify-email/${email}`,
+      { withCredentials: true },
     );
-    // console.log(data.response.data);
-    return data.response.data;
+
+    if (data.message === true) return true;
+    else return false;
+    // return data;
   } catch (err) {
     console.error(err);
   }

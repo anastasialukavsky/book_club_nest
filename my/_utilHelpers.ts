e78bce81162c141430387c5015f2846c3c61f932
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-22 18:45:24 -0500

    rm comments, add default false return in catch block

--- my/_utilHelpers.ts
+++ my/_utilHelpers.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const HTTP_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
 
-export async function emailCheck(email: string) {
+export async function emailCheck(email: string): Promise<boolean> {
   try {
     const { data } = await axios.get(
       `${HTTP_ENDPOINT}/auth/verify-email/${email}`,
@@ -11,8 +11,8 @@ export async function emailCheck(email: string) {
 
     if (data.message === true) return true;
     else return false;
-    // return data;
   } catch (err) {
     console.error(err);
+    return false; //default return
   }
 }

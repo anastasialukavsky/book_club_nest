0725ccd8719066be1a413b370d60cd34688b370a
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-22 20:57:53 -0500

    add credentials

--- my/_utilHelpers.ts
+++ my/_utilHelpers.ts
@@ -5,12 +5,13 @@ const HTTP_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
 export async function emailCheck(email: string) {
   try {
     const { data } = await axios.get(
-      `${HTTP_ENDPOINT}/auth/verify-email/${email}}`,
-      // { withCredentials: true },
+      `${HTTP_ENDPOINT}/auth/verify-email/${email}`,
+      { withCredentials: true },
     );
-    // console.log(data.response.data);
+    console.log(data.response.data);
     return data.response.data;
   } catch (err) {
     console.error(err);
+    
   }
 }

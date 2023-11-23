61cb16c18b47a986f4facc5bcb34c718d86312b8
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-22 21:20:25 -0500

    return bool from email checker, add default false return

--- my/_utilHelpers.ts
+++ my/_utilHelpers.ts
@@ -2,16 +2,17 @@ import axios from 'axios';
 
 const HTTP_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
 
-export async function emailCheck(email: string) {
+export async function emailCheck(email: string): Promise<boolean> {
   try {
     const { data } = await axios.get(
       `${HTTP_ENDPOINT}/auth/verify-email/${email}`,
       { withCredentials: true },
     );
-    console.log(data.response.data);
-    return data.response.data;
+    console.log(data);
+    if (data.message) return true;
+    else return false;
   } catch (err) {
+    return false;
     console.error(err);
-    
   }
 }

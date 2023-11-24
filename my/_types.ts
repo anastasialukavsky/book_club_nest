eb66c19bb60c6c62e0b7e19ad138333e4a1a3580
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-23 23:08:52 -0500

    add util signup user obj type

--- my/_types.ts
+++ my/_types.ts
@@ -2,3 +2,11 @@ export type LoginFormData = {
   email: string;
   password: string;
 };
+
+export type SignUpFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};

80446d7db0c7c458fd1fe090490cd0032ba2ce7b
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-23 23:10:46 -0500

    comment out signup for err testing

--- my/src/app/(auth)/signup/page.tsx
+++ my/src/app/(auth)/signup/page.tsx
@@ -1,15 +1,43 @@
 'use client';
 import React from 'react';
-import visibleIcon from '../../../../public/svg/visible_input.svg';
-import hiddenIcon from '../../../../public/svg/hidden_input.svg';
+// import visibleIcon from '../../../../public/svg/visible_input.svg';
+// import hiddenIcon from '../../../../public/svg/hidden_input.svg';
+// import { useShowPassword } from '@/app/_hooks/useShowPassword';
+// import { useRouter } from 'next/router';
+// import { useForm } from 'react-hook-form';
+// import { zodResolver } from '@hookform/resolvers/zod';
+// import { zodSignup } from '../../../../_zodTypes';
+// import { SignUpFormData } from '../../../../_types';
+// import Image from 'next/image';
+
 export default function Page() {
-  // const [showPassword, setShowPassword]
+  // const router = useRouter();
+  // const { showPassword, togglePasswordVisibility } = useShowPassword();
+
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   reset,
+  //   setError,
+  //   clearErrors,
+  //   setValue,
+  //   formState: { errors },
+  // } = useForm<SignUpFormData>({
+  //   resolver: zodResolver(zodSignup),
+  //   defaultValues: {
+  //     firstName: '',
+  //     lastName: '',
+  //     email: '',
+  //     password: '',
+  //     confirmPassword: '',
+  //   },
+  // });
   return (
     <section
       id="signup-section"
       className="bg-pink-100 w-[100vw] h-[100vh] flex text-slate-700"
     >
-      <div
+      {/* <div
         id="form-wrapper"
         className="self-center mx-auto  h-fit w-[15vw] border border-black"
       >
@@ -27,11 +55,30 @@ export default function Page() {
           <div className="w-full flex flex-col ">
             <label htmlFor="password">password</label>
             <input type="text" />
+            <Image
+              src={showPassword ? visibleIcon : hiddenIcon}
+              width={15}
+              height={15}
+              alt="eye"
+              onClick={togglePasswordVisibility}
+              className="cursor-pointer self-end -translate-y-5 -translate-x-1"
+            />
             <label htmlFor="confirm-password">confirm password</label>
             <input type="text" />
+            <Image
+              src={showPassword ? visibleIcon : hiddenIcon}
+              width={15}
+              height={15}
+              alt="eye"
+              onClick={togglePasswordVisibility}
+              className="cursor-pointer self-end -translate-y-5 -translate-x-1"
+            />
           </div>
+          <button className="mt-3 w-full h-fit border border-black font-medium">
+            login
+          </button>
         </form>
-      </div>
+      </div> */}
     </section>
   );
 }

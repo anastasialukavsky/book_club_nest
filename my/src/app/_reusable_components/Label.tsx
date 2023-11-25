752f37b4f4a6d7ef115d8411d6e5d173d79a2d81
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-24 23:12:45 -0500

    add swith cases for 2 word labels to display them separately

--- my/src/app/_reusable_components/Label.tsx
+++ my/src/app/_reusable_components/Label.tsx
@@ -5,9 +5,27 @@ interface LabelProps {
 }
 
 export default function Label({ htmlFor, label }: LabelProps) {
+  let formattedLabel: string;
+
+  switch (label) {
+    case 'firstName':
+      formattedLabel = 'first name';
+      break;
+    case 'lastName':
+      formattedLabel = 'last name';
+      break;
+    case 'confirmPassword':
+      formattedLabel = 'confirm password';
+      break;
+
+    default:
+      formattedLabel = label;
+      break;
+  }
+
   return (
     <label htmlFor={htmlFor} className="mb-1">
-      {label}
+      {formattedLabel}
     </label>
   );
 }
